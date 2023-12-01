4851a766b98b94a93322743801f709cf345c6692
Kamil01 <107310243+Adminek01@users.noreply.github.com>
2023-12-01 09:25:01 +0100

    Update ddos-onion.py

--- ddos-onion.py
+++ ddos-onion.py
@@ -4,7 +4,8 @@ import random
 import logging
 
 DEFAULT_USER_AGENTS = [
-    # ... (other user-agents)
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
+    # Add more user agents as needed
 ]
 
 DEFAULT_SOCK_TIMEOUT = 4
